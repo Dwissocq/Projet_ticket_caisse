929e6f42bf37e093d76200b8146bbe9c657bf4df
David Wissocq <dwissocq@gmail.com>
2021-03-03 13:38:06 +0100

    Ajout de la facture

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,9 +8,13 @@ public class Main {
         // On déclare un scanner pour lire les entrées de la console
         Scanner sc = new Scanner(System.in);
 
+        //Définir les variables
+        float prixtotal=0;
+        String listeCommande="";
+
         // Boucle while tant qu'il y a un autre plat
         int index = 1;
-        while (index == 1){
+                while (index == 1){
 
             // On demande le plat
             System.out.println("Entrez plat : ");
@@ -28,6 +32,11 @@ public class Main {
             float px1 = sc.nextFloat();
             sc.nextLine();
 
+            //On calcule le prix total
+            prixtotal =prixtotal + (q1 * px1);
+
+            listeCommande= q1+" x "+p1+" ( "+px1+ "€) "+q1*px1+"€\n"+ listeCommande;
+
             // On demande s'il y a un autre plat
             System.out.println("Y a t il une autre commande ? Oui = 1 / Non = 0");
 
@@ -36,6 +45,11 @@ public class Main {
             sc.nextLine();
 
         }
+
+        System.out.println("----------------FACTURE----------------");
+        System.out.println(listeCommande);
+        System.out.println("Prix total de "+prixtotal+"€");
+        System.out.println("-------------BONNE JOURNEE-------------");
 
         // On ferme le scanner
         sc.close();
