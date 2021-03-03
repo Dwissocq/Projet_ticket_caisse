5cfb31e382628cf539f6831fa8e4365782c3c75b
David Wissocq <dwissocq@gmail.com>
2021-03-03 11:31:18 +0100

    Boucle faite

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,34 +8,33 @@ public class Main {
         // On déclare un scanner pour lire les entrées de la console
         Scanner sc = new Scanner(System.in);
 
-        // On demande le plat
-        System.out.println("Entrez plat : ");
+        // Boucle while tant qu'il y a un autre plat
+        int index = 1;
+        while (index == 1){
 
-        // On récupère le plat
-        String p1 = sc.nextLine();
+            // On demande le plat
+            System.out.println("Entrez plat : ");
 
-        // On demande la quantité
-        System.out.println("Entrez la quantité : ");
-        int q1 = sc.nextInt();
-        sc.nextLine();
+            // On récupère le plat
+            String p1 = sc.nextLine();
 
-        // On inscrit le prix
-        System.out.println("Entrez le prix");
-        float px1 = sc.nextFloat();
-        sc.nextLine();
+            // On demande la quantité
+            System.out.println("Entrez la quantité : ");
+            int q1 = sc.nextInt();
+            sc.nextLine();
 
-        // On demande s'il y a un autre plat
-        System.out.println("Commande suivante :");
+            // On inscrit le prix
+            System.out.println("Entrez le prix");
+            float px1 = sc.nextFloat();
+            sc.nextLine();
 
-        //On récupère le plat2
-        String p2 = sc.nextLine();
+            // On demande s'il y a un autre plat
+            System.out.println("Y a t il une autre commande ? Oui = 1 / Non = 0");
 
-        if (p2="") {
-            //On affiche l'addition
-            System.out.println(q1 + " x " + p1 + " " + px1 + "euros");
-        } else {
-            //A développer, en attendant mettre fin
-            System.out.println("A suivre");
+            // On récupère la réponse
+            index = sc.nextInt();
+            sc.nextLine();
+
         }
 
         // On ferme le scanner
